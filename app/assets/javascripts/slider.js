6abf7e24b90dab4d72f01dd5048fe43b845ad209
andy <andy@andys-MacBook-Pro.local>
2013-02-03 11:55:01 -0500

    fixed slider bug

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  var max_pages = parseInt($("#video-slider-list").attr('data-maxspots'));
+
+  $("#move-left").click(function(e) {
+    e.preventDefault();
+    current_spot = parseInt($("#video-slider-list").attr('data-current'));
+    turnToSpot(current_spot - 1);
+  })
+
+  $("#move-right").click(function(e) {
+    e.preventDefault();
+    current_spot = parseInt($("#video-slider-list").attr('data-current'));
+    turnToSpot(current_spot + 1);
+  })
+
+  $(".video-page").click(function(e) {
+    e.preventDefault();
+    new_spot = parseInt($(this).attr('data-id'));
+    turnToSpot(new_spot);
+  });
+
+  function turnToSpot(new_spot){
+    if(new_spot >= 1 && new_spot <= max_pages){
+      distance_interval = 600
+
+      new_position = distance_interval * (new_spot -1)
+
+      $("#video-slider-list").attr('data-current', new_spot)
+
+      $('#video-slider-list').animate({
+          right: new_position}, 300, 'swing');
+
+      $(".video-page").removeClass("active-video-page");
+      $(".video-page[data-id='" + new_spot +"']").addClass("active-video-page")
+    }
+  };
+});
+
