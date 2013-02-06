336366b4aa73406a8fc56bbc8d4864b32c4ebd35
andy <andy@andys-MacBook-Pro.local>
2013-02-05 19:48:54 -0500

    added new video.  started resume

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/The_Collection.jpg", :link => "http://www.youtube.com/embed/lds5ws-d_hk?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Powershift_Still.jpg", :link => "http://www.youtube.com/embed/Gn0KYPe8glA?autoplay=1")
+Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/react_to_film.jpg", :link => "http://www.youtube.com/embed/H620VHaNjfA?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Bike_Movie_Still.jpg", :link => "http://www.youtube.com/embed/96oZnzLjcL8?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Petz_Movie_Still.jpg", :link => "http://www.youtube.com/embed/GI1ZfN4HIfw?autoplay=1")
-Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Windmill_Still.jpg", :link => "http://www.youtube.com/embed/IdSPBvznwaQ?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/OWS_Movie_Still.jpg", :link => "http://www.youtube.com/embed/f2kmkNZObBM?autoplay=1")
+Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Windmill_Still.jpg", :link => "http://www.youtube.com/embed/IdSPBvznwaQ?autoplay=1")
+
