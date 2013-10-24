af3e6831f109217bff0f98d115bd4b20a147c029
Andrew Glass <andy.glass@livingsocial.com>
2013-10-23 22:16:59 -0400

    add/remove videos, remove cv, background color

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
+Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Bad_Medicine.png", :link => "http://www.youtube.com/embed/QQasz9bfXxQ?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/The_Collection.jpg", :link => "http://www.youtube.com/embed/lds5ws-d_hk?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Powershift_Still.jpg", :link => "http://www.youtube.com/embed/Gn0KYPe8glA?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/react_to_film.jpg", :link => "http://www.youtube.com/embed/H620VHaNjfA?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Bike_Movie_Still.jpg", :link => "http://www.youtube.com/embed/96oZnzLjcL8?autoplay=1")
 Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Petz_Movie_Still.jpg", :link => "http://www.youtube.com/embed/GI1ZfN4HIfw?autoplay=1")
-Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/OWS_Movie_Still.jpg", :link => "http://www.youtube.com/embed/f2kmkNZObBM?autoplay=1")
-Video.create(:photo => "https://s3.amazonaws.com/lindseyeve/Windmill_Still.jpg", :link => "http://www.youtube.com/embed/IdSPBvznwaQ?autoplay=1")
-
