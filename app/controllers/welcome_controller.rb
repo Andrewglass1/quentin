79f01992b83b04ae432155e6d57e27e5e1d332dc
andy <andy@andys-MacBook-Pro.local>
2013-10-24 23:39:03 -0400

    new site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,17 @@
 class WelcomeController < ApplicationController
 
+  VIDEOS = [{:video => "https://vimeo.com/77183634&output=embed", :image => "http://assets.nydailynews.com/polopoly_fs/1.1329359!/img/httpImage/image.jpg_gen/derivatives/landscape_635/geno28s-2-web.jpg"},
+            {:video => "https://vimeo.com/77183634", :image => "http://assets.nydailynews.com/polopoly_fs/1.1329359!/img/httpImage/image.jpg_gen/derivatives/landscape_635/geno28s-2-web.jpg"},
+            {:video => "https://vimeo.com/77183634", :image => "http://assets.nydailynews.com/polopoly_fs/1.1329359!/img/httpImage/image.jpg_gen/derivatives/landscape_635/geno28s-2-web.jpg"},
+            {:video => "https://vimeo.com/77183634", :image => "http://assets.nydailynews.com/polopoly_fs/1.1329359!/img/httpImage/image.jpg_gen/derivatives/landscape_635/geno28s-2-web.jpg"},
+            {:video => "https://vimeo.com/77183634", :image => "http://assets.nydailynews.com/polopoly_fs/1.1329359!/img/httpImage/image.jpg_gen/derivatives/landscape_635/geno28s-2-web.jpg"},
+            {:video => "https://vimeo.com/77183634", :image => "http://assets.nydailynews.com/polopoly_fs/1.1329359!/img/httpImage/image.jpg_gen/derivatives/landscape_635/geno28s-2-web.jpg"}
+            ]
   def index
     @videos = Video.all
+  end
+
+  def new
+    @videos = VIDEOS
   end
 end
