c81600b6a9283c0b5f4d19635e2f470cc59ba4c0
Andrew Glass <andy.glass@livingsocial.com>
2013-10-27 23:46:01 -0400

    support for youtube and vimeo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,12 @@
 class WelcomeController < ApplicationController
 
   VIDEOS = [
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"},
-            {:vimeo_id => 77183634, :image => "http://b.vimeocdn.com/ts/452/295/452295896_640.jpg", :title => "Bad Medicine"}
+            {:type => "vimeo", :id => 77183634, :image => "https://s3.amazonaws.com/lindseyeve/bad_medicine.png", :title => "Bad Medicine"},
+            {:type => "vimeo", :id => 77814644, :image => "https://s3.amazonaws.com/lindseyeve/the_collection.png", :title => "The Collection"},
+            # {:type => "vimeo", :id => , :image => "https://s3.amazonaws.com/lindseyeve/last_film_festival.png", :title => "The Last Film Festival Promo"},
+            {:type => "vimeo", :id => 77189031, :image => "https://s3.amazonaws.com/lindseyeve/graphics_reel.png", :title => "Graphics Reel"},
+            {:type => "youtube", :id => "oLn54cqwlI8", :image => "https://s3.amazonaws.com/lindseyeve/house_i_live_in.png", :title => "The House I Live In Q & A"},
+            {:type => "youtube", :id => "96oZnzLjcL8", :image => "https://s3.amazonaws.com/lindseyeve/bikes.png", :title => "<3 Bikes Vol. 1"},
            ]
 
   def index
