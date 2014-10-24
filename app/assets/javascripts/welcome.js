8422003302cd9c6e301639a070d2db4dc913fddf
Andrew Glass <andrewglass1@gmail.com>
2014-10-24 18:35:56 -0400

    move some js out of application layout

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+
   $('#contact-text').bigtext();
+
+  Shadowbox.init({
+      handleOversize: "resize",
+      initialWidth: 600,
+      initialHeight: 400,
+      overlayOpacity: .8,
+      resizeDuration: .3,
+      animSequence: "sync"
+  });
 });
